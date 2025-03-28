4d4cc667adea899430a59f21d6d27b16a4801bf9
xzyy <xueayi503@qq.com>
2025-03-28 11:18:53 +0800

    更新网站地址com和备案号-2

--- blog.config.js
+++ blog.config.js
@@ -16,10 +16,10 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '雪阿宜', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的游戏玩家🎮', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.xueayi.site/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.xueayi.com/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
-  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '蜀ICP备2024067523号-1', // 备案号 闽ICP备XXXXXX
+  BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '蜀ICP备2024067523号-2', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
 
   // RSS订阅
